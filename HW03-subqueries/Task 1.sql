3ddeda4c8fa46256eb1ba068919455cdaed409b5
kalekinata <67870913+kalekinata@users.noreply.github.com>
2020-07-16 16:17:19 +0300

    Update Task 1.sql

--- HW03-subqueries/Task 1.sql
+++ HW03-subqueries/Task 1.sql
@@ -1,4 +1,5 @@
-﻿/*1. Выберите сотрудников (Application.People), которые являются продажниками (IsSalesPerson), и не сделали ни одной продажи 04 июля 2015 года. Вывести ИД сотрудника и его полное имя. Продажи смотреть в таблице Sales.Invoices.*/
+/*1. Выберите сотрудников (Application.People), которые являются продажниками (IsSalesPerson), и не сделали 
+ ни одной продажи 04 июля 2015 года. Вывести ИД сотрудника и его полное имя. Продажи смотреть в таблице Sales.Invoices.*/
 SELECT PersonID, FullName
 FROM Application.People
 WHERE IsSalesperson = (SELECT CustomerID
