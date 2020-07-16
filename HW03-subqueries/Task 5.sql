c38e04f5f630cb43133badbe7a0aca6a31169659
kalekinata <67870913+kalekinata@users.noreply.github.com>
2020-07-16 16:20:11 +0300

    Update Task 5.sql

--- HW03-subqueries/Task 5.sql
+++ HW03-subqueries/Task 5.sql
@@ -9,9 +9,9 @@ SELECT
 	(SELECT SUM(OrderLines.PickedQuantity*OrderLines.UnitPrice)
 		FROM Sales.OrderLines
 		WHERE OrderLines.OrderId = (SELECT Orders.OrderId
-									FROM Sales.Orders
-									WHERE Orders.PickingCompletedWhen IS NOT NULL
-									AND Orders.OrderId = Invoices.OrderId)) AS TotalSummForPickedItems
+						FROM Sales.Orders
+						WHERE Orders.PickingCompletedWhen IS NOT NULL
+						AND Orders.OrderId = Invoices.OrderId)) AS TotalSummForPickedItems
 FROM Sales.Invoices
 JOIN(SELECT InvoiceId, SUM(Quantity*UnitPrice) AS TotalSumm
 		FROM Sales.InvoiceLines
@@ -19,7 +19,9 @@ JOIN(SELECT InvoiceId, SUM(Quantity*UnitPrice) AS TotalSumm
 		HAVING SUM(Quantity*UnitPrice) > 27000) AS SalesTotals
 		ON Invoices.InvoiceID = SalesTotals.InvoiceID
 		ORDER BY TotalSumm DESC
-Можно двигаться как в сторону улучшения читабельности запроса, так и в сторону упрощения плана\ускорения. Сравнить производительность запросов можно через SET STATISTICS IO, TIME ON. Если знакомы с планами запросов, то используйте их (тогда к решению также приложите планы). Напишите ваши рассуждения по поводу оптимизации.*/
+Можно двигаться как в сторону улучшения читабельности запроса, так и в сторону упрощения плана\ускорения.
+Сравнить производительность запросов можно через SET STATISTICS IO, TIME ON. Если знакомы с планами запросов, то используйте их (тогда к решению также приложите планы).
+Напишите ваши рассуждения по поводу оптимизации.*/
 
 SELECT Invoices.InvoiceID,Invoices.InvoiceDate,People.FullName AS SalesPersonName, SalesTotals.TotalSumm AS TotalSummByInvoice,
 	(SELECT SUM(OrderLines.PickedQuantity*OrderLines.UnitPrice)
@@ -34,4 +36,6 @@ JOIN (SELECT InvoiceId, SUM(Quantity*UnitPrice) AS TotalSumm
 		HAVING SUM(Quantity*UnitPrice) > 27000) AS SalesTotals ON Invoices.InvoiceID = SalesTotals.InvoiceID
 ORDER BY TotalSumm DESC
 
-Необходимо выбрать id счёта, дату составления счёта, имя продавца, который является продажником, оформляющим счета. Также вывести общую сумму оформленного заказа, которая больше 27000, и общую цену упакованного товара при завершении комплектации заказа. Информация выводится в порядке убывания общей суммы заказа.
+Необходимо выбрать id счёта, дату составления счёта, имя продавца, который является продажником, оформляющим счета.
+Также вывести общую сумму оформленного заказа, которая больше 27000, и общую цену упакованного товара при завершении комплектации заказа.
+Информация выводится в порядке убывания общей суммы заказа.
