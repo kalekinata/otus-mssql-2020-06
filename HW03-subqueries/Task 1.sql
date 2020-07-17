c8caac360a5b411a0c8fa7fde8612cff034fdbd0
kalekinata <67870913+kalekinata@users.noreply.github.com>
2020-07-17 10:43:47 +0300

    Update Task 1.sql

--- HW03-subqueries/Task 1.sql
+++ HW03-subqueries/Task 1.sql
@@ -1,7 +1,7 @@
 /*1. Выберите сотрудников (Application.People), которые являются продажниками (IsSalesPerson), и не сделали 
  ни одной продажи 04 июля 2015 года. Вывести ИД сотрудника и его полное имя. Продажи смотреть в таблице Sales.Invoices.*/
-SELECT PersonID, FullName
-FROM Application.People
-WHERE IsSalesperson = (SELECT CustomerID
-						FROM Sales.Invoices
-						WHERE (InvoiceID IS NULL) AND InvoiceDate = '2015-07-04')
+SELECT p.PersonID, p.FullName
+FROM Application.People p
+WHERE EXISTS(SELECT *
+		FROM Sales.Invoices i
+		WHERE p.IsSalesperson = i.CustomerID AND (InvoiceID IS NULL) AND InvoiceDate = '2015-07-04')
