8c1de1d1c2afe089d3d000239bc41c1aa957ce76
kalekinata <67870913+kalekinata@users.noreply.github.com>
2020-10-31 18:45:08 +0300

    Update Task 1.sql

--- HW09-xml, json/Task 1.sql
+++ HW09-xml, json/Task 1.sql
@@ -1,3 +1,7 @@
+1. Загрузить данные из файла StockItems.xml в таблицу Warehouse.StockItems.
+Существующие записи в таблице обновить, отсутствующие добавить (сопоставлять записи по полю StockItemName).
+Файл StockItems.xml в личном кабинете.
+
 DECLARE @x XML
 SET @x = ( 
   SELECT * FROM OPENROWSET
