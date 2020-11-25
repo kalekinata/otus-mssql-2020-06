e195162d3da74f86a79fbdf3f4f50b7671c4e9c1
kalekinata <67870913+kalekinata@users.noreply.github.com>
2020-11-25 09:36:27 +0300

    Add files via upload

---
+++ HW15-hint's/Task.sql
@@ -0,0 +1,67 @@
+set statistics io, time  on;
+
+Select ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)
+FROM Sales.Orders AS ord
+INNER MERGE JOIN Sales.OrderLines AS det
+ON det.OrderID = ord.OrderID
+INNER MERGE JOIN Sales.Invoices AS Inv
+ON Inv.OrderID = ord.OrderID
+INNER MERGE JOIN Sales.CustomerTransactions AS Trans
+ON Trans.InvoiceID = Inv.InvoiceID
+INNER MERGE JOIN Warehouse.StockItemTransactions AS ItemTrans
+ON ItemTrans.StockItemID = det.StockItemID
+WHERE Inv.BillToCustomerID != ord.CustomerID
+AND (Select SupplierId
+FROM Warehouse.StockItems AS It
+Where It.StockItemID = det.StockItemID) = 12
+AND (SELECT SUM(Total.UnitPrice*Total.Quantity)
+FROM Sales.OrderLines AS Total
+INNER MERGE JOIN Sales.Orders AS ordTotal
+On ordTotal.OrderID = Total.OrderID
+WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
+AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
+GROUP BY ord.CustomerID, det.StockItemID
+ORDER BY ord.CustomerID, det.StockItemID
+option(maxdop 1)
+
+/*Время синтаксического анализа и компиляции SQL Server: 
+ время ЦП = 0 мс, истекшее время = 0 мс.
+
+ Время работы SQL Server:
+   Время ЦП = 0 мс, затраченное время = 0 мс.
+
+ Время работы SQL Server:
+   Время ЦП = 0 мс, затраченное время = 0 мс.
+Внимание! Порядок данного соединения был выбран принудительно, поскольку использовалось указание локального соединения.
+Время синтаксического анализа и компиляции SQL Server: 
+ время ЦП = 24 мс, истекшее время = 24 мс.
+
+ Время работы SQL Server:
+   Время ЦП = 0 мс, затраченное время = 0 мс.
+
+(затронуто строк: 3619)
+Таблица "OrderLines". Число просмотров 4, логических чтений 0, физических чтений 0, упреждающих чтений 0, lob логических чтений 331, lob физических чтений 0, lob упреждающих чтений 0.
+Таблица "OrderLines". Считано сегментов 2, пропущено 0.
+Таблица "Worktable". Число просмотров 15, логических чтений 62132, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+Таблица "StockItemTransactions". Число просмотров 1, логических чтений 60, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+Таблица "CustomerTransactions". Число просмотров 5, логических чтений 261, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+Таблица "Orders". Число просмотров 2, логических чтений 1384, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+Таблица "Invoices". Число просмотров 1, логических чтений 11400, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+Таблица "StockItems". Число просмотров 1, логических чтений 2, физических чтений 0, упреждающих чтений 0, lob логических чтений 0, lob физических чтений 0, lob упреждающих чтений 0.
+
+(затронуто строк: 31)
+
+(затронута одна строка)
+
+ Время работы SQL Server:
+   Время ЦП = 2313 мс, затраченное время = 2398 мс.
+Время синтаксического анализа и компиляции SQL Server: 
+ время ЦП = 0 мс, истекшее время = 0 мс.
+
+ Время работы SQL Server:
+   Время ЦП = 0 мс, затраченное время = 0 мс.
+
+ Время работы SQL Server:
+   Время ЦП = 0 мс, затраченное время = 0 мс.*/
+
+
